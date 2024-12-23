0f0ae645959e0033b46702bd7969b4d4e95ced9f
Kooreis <kooreis.official@gmail.com>
2024-12-22 22:29:24 -0600

    This update includes the import statements, class declaration, and the initialization of constants and variables. It also sets up the main method and initializes the game board and mines array.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a simple implementation of Minesweeper in Java:
-
-```java
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,41 +13,3 @@ public class Main {
         int row, col;
         Random random = new Random();
         Scanner input = new Scanner(System.in);
-
-        for (int i = 0; i < SIZE; i++)
-            for (int j = 0; j < SIZE; j++) {
-                board[i][j] = HIDDEN;
-                if (random.nextInt(7) == 0)
-                    mines[i][j] = MINE;
-                else
-                    mines[i][j] = EMPTY;
-            }
-
-        do {
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++)
-                    System.out.print(board[i][j]);
-                System.out.println();
-            }
-
-            System.out.print("Enter your move (row[1-10] column[1-10]): ");
-            row = input.nextInt() - 1;
-            col = input.nextInt() - 1;
-
-            if (mines[row][col] == MINE) {
-                System.out.println("You lost");
-                break;
-            } else {
-                int count = 0;
-                for (int i = -1; i <= 1; i++)
-                    for (int j = -1; j <= 1; j++)
-                        if (mines[row + i][col + j] == MINE)
-                            count++;
-                board[row][col] = (char) (count + '0');
-            }
-        } while (true);
-    }
-}
-```
-
-This program creates a 10x10 board and randomly places mines on it. The user is asked to enter a row and column number. If the chosen cell contains a mine, the game ends. If not, the number of mines in the adjacent cells is calculated and displayed. The game continues until the user hits a mine.
