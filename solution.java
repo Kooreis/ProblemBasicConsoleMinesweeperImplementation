aba628328a8342e8cc20b231171b08550ce7d0ec
Kooreis <kooreis.official@gmail.com>
2024-12-23 23:29:24 -0600

    This update includes the code that populates the game board and mines array. It sets all cells on the board to hidden and randomly places mines on the mines array.

--- solution.java
+++ solution.java
@@ -1,15 +1,8 @@
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main {
-    private static final char EMPTY = ' ';
-    private static final char MINE = '*';
-    private static final char HIDDEN = '.';
-    private static final int SIZE = 10;
-
-    public static void main(String[] args) {
-        char[][] board = new char[SIZE][SIZE];
-        char[][] mines = new char[SIZE][SIZE];
-        int row, col;
-        Random random = new Random();
-        Scanner input = new Scanner(System.in);
+for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = HIDDEN;
+                if (random.nextInt(7) == 0)
+                    mines[i][j] = MINE;
+                else
+                    mines[i][j] = EMPTY;
+            }
